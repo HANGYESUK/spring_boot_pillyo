55667a131aebe1a6fa36f6b24332fa7348544dce
bomooline <bomooline@naver.com>
2022-02-10 15:55:23 +0900

    test1.제발..

--- spring_boot_pillyo/src/main/java/com/pillyo/pill/dao/IMemberDAO.java
+++ spring_boot_pillyo/src/main/java/com/pillyo/pill/dao/IMemberDAO.java
@@ -1,5 +1,12 @@
 package com.pillyo.pill.dao;
 
+import java.util.HashMap;
+
+import com.pillyo.pill.model.MemberVO;
+
 public interface IMemberDAO {
 
+	// 로그인 체크
+	MemberVO loginCheck(HashMap<String, Object> map);
+	String userIdCheck(String userId);
 }
