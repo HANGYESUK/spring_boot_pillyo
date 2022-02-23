2a42ee2c9dd0403bab1e644428a50cb5eb468011
[hyovvely] <[kkd94793@naver.com]>
2022-02-23 10:39:05 +0900

    게시판 CSS + CRUD 수정/추가

--- spring_boot_pillyo/src/main/java/com/pillyo/pill/model/BoardVO.java
+++ spring_boot_pillyo/src/main/java/com/pillyo/pill/model/BoardVO.java
@@ -1,36 +1,30 @@
 package com.pillyo.pill.model;
 
 public class BoardVO {
-	private int id;
-	private String title;
-	private String content;
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
 	private String userId;
-	private String joinDate;
-	private String boardCtg;
-	
-	public String getBoardCtg() {
-		return boardCtg;
+	private String boardWriteDate;
+	private String boardCtgNo;
+	private String boardImg;
+	public int getBoardNo() {
+		return boardNo;
 	}
-	public void setBoardCtg(String boardCtg) {
-		this.boardCtg = boardCtg;
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
 	}
-	public int getId() {
-		return id;
+	public String getBoardTitle() {
+		return boardTitle;
 	}
-	public void setId(int id) {
-		this.id = id;
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
 	}
-	public String getTitle() {
-		return title;
+	public String getBoardContent() {
+		return boardContent;
 	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
 	}
 	public String getUserId() {
 		return userId;
@@ -38,11 +32,23 @@ public class BoardVO {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	public String getJoinDate() {
-		return joinDate;
+	public String getBoardWriteDate() {
+		return boardWriteDate;
 	}
-	public void setJoinDate(String joinDate) {
-		this.joinDate = joinDate;
+	public void setBoardWriteDate(String boardWriteDate) {
+		this.boardWriteDate = boardWriteDate;
+	}
+	public String getBoardCtgNo() {
+		return boardCtgNo;
+	}
+	public void setBoardCtgNo(String boardCtgNo) {
+		this.boardCtgNo = boardCtgNo;
+	}
+	public String getBoardImg() {
+		return boardImg;
+	}
+	public void setBoardImg(String boardImg) {
+		this.boardImg = boardImg;
 	}
 	
 
